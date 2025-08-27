6deb373f895f59a97e75b3ba7e8c8cfc886e23ef
idrislabdev <idrislabdev@gmail.com>
2025-08-27 14:49:34 +0700

    update

--- src/@core/pages/payment/components/tab-page/index.tsx
+++ src/@core/pages/payment/components/tab-page/index.tsx
@@ -1,23 +1,16 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from 'next/link'
+import React from 'react'
 
-const PengaturanTabpage = (props: { activeTab: string }) => {
-  const { activeTab } = props;
+const PaymentTabPage = (props: {activeTab:string}) => {
+  const { activeTab } = props
   return (
-    <div className="tab-default">
-      <ul>
-        <li className={`${activeTab === 'admin' ? 'active' : ''}`}>
-          <Link href={`/pengaturan/admin`}>Data Admin</Link>
-        </li>
-        <li className={`${activeTab === 'admin_fee' ? 'active' : ''}`}>
-          <Link href={`/pengaturan/admin-fee`}>Admin Fee</Link>
-        </li>
-        <li className={`${activeTab === 'investment_return' ? 'active' : ''}`}>
-          <Link href={`/pengaturan/investment-return`}>Investment Return</Link>
-        </li>
-      </ul>
+    <div className='tab-default'>
+        <ul>
+            <li className={`${activeTab === 'bank' ? 'active' : ''}`}><Link href={`/payment/bank`}>Data Bank</Link></li>
+            <li className={`${activeTab === 'payment_method' ? 'active' : ''}`}><Link href={`/payment/payment-method`}>Payment Method</Link></li>
+        </ul>
     </div>
-  );
-};
+  )
+}
 
-export default PengaturanTabpage;
+export default PaymentTabPage
