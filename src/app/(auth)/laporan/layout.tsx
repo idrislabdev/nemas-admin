e78b605e98e6163b7339ba67973b92f9687aff7d
mochamad <inoreds@gmail.com>
2025-09-13 11:30:03 +0700

    update

---
+++ src/app/(auth)/laporan/layout.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { IUser } from '@/@core/@types/interface';
+import React from 'react';
+
+const LaporanLayout = ({ children }: { children: React.ReactNode }) => {
+  const user: IUser = JSON.parse(localStorage.getItem('user') || '{}');
+
+  return (
+    <>
+      {user && user.menus.some((x) => x.name === 'Laporan' && x.accessible) ? (
+        children
+      ) : (
+        <div className="main-container flex flex-col justify-center items-center">
+          <h5>Not Authorize</h5>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default LaporanLayout;
