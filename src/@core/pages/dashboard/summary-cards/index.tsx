df6d2958d1fe42e3a702143af0b7d6a00a7a46d9
idrislabdev <idrislabdev@gmail.com>
2025-10-29 06:03:38 +0700

    update: on progress dashboard

--- src/@core/pages/dashboard/summary-cards/index.tsx
+++ src/@core/pages/dashboard/summary-cards/index.tsx
@@ -21,27 +21,27 @@ const SummaryCards = () => {
 
   return (
     <div className="flex items-center gap-[8px]">
-      <div className="w-1/3 flex flex-col justify-between h-[120px] shadow-custom-1 rounded-md p-4">
-        <label className="text-xl leading-5 text-green-700 font-semibold line-clamp-2">
+      <div className="w-1/3 flex flex-col justify-center h-[120px] shadow-custom-1 rounded-md p-4">
+        <label className="text-2xl text-green-700 font-semibold line-clamp-2">
           Total Pengguna Aktif
         </label>
-        <span className="text-neutral-700 text-[21px]/[23px] font-medium">
+        <span className="text-neutral-700 text-xl font-medium">
           {formatterNumber(data.total_active_user)}
         </span>
       </div>
-      <div className="w-1/3 flex flex-col justify-between h-[120px] shadow-custom-1 rounded-md p-4">
-        <label className="text-xl leading-5 text-green-700 font-semibold line-clamp-2">
+      <div className="w-1/3 flex flex-col justify-center h-[120px] shadow-custom-1 rounded-md p-4">
+        <label className="text-2xl text-green-700 font-semibold line-clamp-2">
           Total Pengguna Keseluruhan
         </label>
-        <span className="text-neutral-700 text-[21px]/[23px] font-medium">
+        <span className="text-neutral-700 text-xl font-medium">
           {formatterNumber(data.total_user)}
         </span>
       </div>
-      <div className="w-1/3 flex flex-col justify-between h-[120px] shadow-custom-1 rounded-md p-4">
-        <label className="text-xl leading-5 text-green-700 font-semibold line-clamp-2">
+      <div className="w-1/3 flex flex-col justify-center h-[120px] shadow-custom-1 rounded-md p-4">
+        <label className="text-2xl text-green-700 font-semibold line-clamp-2">
           Total Toko Terdaftar
         </label>
-        <span className="text-neutral-700 text-[21px]/[23px] font-medium">
+        <span className="text-neutral-700 text-xl font-medium">
           {formatterNumber(data.total_toko_user)}
         </span>
       </div>
