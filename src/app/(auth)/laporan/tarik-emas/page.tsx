c8e79b18d9fb120d74bd17c2c6ff0c512d6f3a05
idrislabdev <idrislabdev@gmail.com>
2025-12-02 20:53:44 +0700

    update: tarik emas page

---
+++ src/app/(auth)/laporan/tarik-emas/page.tsx
@@ -0,0 +1,46 @@
+import ProfileDropdown from '@/@core/components/profile-dropdown';
+import { ArrowLeftIcon, FolderIcon, FolderOpenIcon } from '@/@core/my-icons';
+import { Printer } from '@untitled-ui/icons-react';
+
+export default function LaporanTarikEmasPage() {
+  return (
+    <div className="main-container">
+      <div className="container-header">
+        <div className="breadcrumb-info">
+          <ul>
+            <li>
+              <ArrowLeftIcon />
+            </li>
+            <li>
+              <FolderIcon /> Home
+            </li>
+            <li>
+              <FolderIcon /> Laporan
+            </li>
+            <li>
+              <FolderOpenIcon /> Tarik Emas
+            </li>
+          </ul>
+        </div>
+        <ProfileDropdown />
+      </div>
+      <div className="container-body">
+        <div className="title-body">
+          <div className="logo-area">
+            <Printer />
+          </div>
+          <div className="text-area">
+            <h5>Halaman Laporan</h5>
+            <p>Tarik Emas</p>
+          </div>
+        </div>
+        <div className="main-body">
+          <hr />
+          <div className="flex flex-col gap-[10px] justify-center items-center h-full">
+            On Progress
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
