ef1c5ece0f81d43486531d1ee0208306eb1d8a6a
idrislabdev <idrislabdev@gmail.com>
2026-02-17 12:53:08 +0700

    udpate

--- src/@core/pages/laporan/gold-buy/index.tsx
+++ src/@core/pages/laporan/gold-buy/index.tsx
@@ -5,27 +5,10 @@ import GoldBuySummaryUserTable from '@/@core/pages/laporan/gold-buy/table/summar
 import GoldBuyDigitalDetailTable from '@/@core/pages/laporan/gold-buy/table/detail';
 
 const LaporanGoldBuyDigitalPage = () => {
-  const [tabActive, setTabActive] = useState('detail');
+  const [tabActive, setTabActive] = useState('ringkasan');
   return (
     <div className="flex flex-col gap-[20px]">
       <div className="flex items-center gap-[20px]">
-        <div
-          className={`flex items-center gap-[8px] rounded w-1/2 border px-[20px] py-[10px] cursor-pointer ${
-            tabActive === 'detail'
-              ? 'bg-gray-100 text-neutral-600 border-gray-300'
-              : 'border-gray-200'
-          }`}
-          onClick={() => setTabActive('detail')}
-        >
-          <span
-            className={`my-icon icon-xl ${
-              tabActive === 'detail' ? 'text-neutral-600' : 'text-neutral-500'
-            }`}
-          >
-            <CoinsStacked01 />
-          </span>
-          <h5 className="font-semibold text-[17px]/[21px]">Detail Pembelian</h5>
-        </div>
         <div
           className={`flex items-center gap-[8px] rounded w-1/2 border px-[20px] py-[10px] cursor-pointer ${
             tabActive === 'ringkasan'
@@ -44,8 +27,25 @@ const LaporanGoldBuyDigitalPage = () => {
             <Wallet05 />
           </span>
           <h5 className="font-semibold text-[17px]/[21px]">
-            Ringkasan Pembelian
+            Rekapitulasi Pembelian
           </h5>
+        </div>
+        <div
+          className={`flex items-center gap-[8px] rounded w-1/2 border px-[20px] py-[10px] cursor-pointer ${
+            tabActive === 'detail'
+              ? 'bg-gray-100 text-neutral-600 border-gray-300'
+              : 'border-gray-200'
+          }`}
+          onClick={() => setTabActive('detail')}
+        >
+          <span
+            className={`my-icon icon-xl ${
+              tabActive === 'detail' ? 'text-neutral-600' : 'text-neutral-500'
+            }`}
+          >
+            <CoinsStacked01 />
+          </span>
+          <h5 className="font-semibold text-[17px]/[21px]">Detail Pembelian</h5>
         </div>
       </div>
       {tabActive === 'detail' && <GoldBuyDigitalDetailTable />}
